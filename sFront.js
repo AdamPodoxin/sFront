623a2d706afca65a381ac6917cd8f24fd2ccedfb
Adam Podoxin <adam.podoxin@gmail.com>
2020-07-13 16:59:27 -0700

    Allow elements to dynamically update when their variables are changed

--- sFront.js
+++ sFront.js
@@ -1,22 +1,72 @@
 export const createElementFromTemplate = (
   templatePath,
   variables,
-  callback,
-  insertAtElement = null
+  insertAtElement,
+  callback = null
 ) => {
   let xhr = new XMLHttpRequest();
   xhr.onload = () => {
     let responseText = xhr.responseText;
 
-    for (let key in variables) {
-      const keyInRes = `$${key}`;
-      responseText = responseText.replace(keyInRes, variables[key]);
-    }
-
-    if (insertAtElement !== null) insertAtElement.innerHTML += responseText;
-    if (callback !== null) callback(responseText);
+    let elementObject = createElementObject(
+      responseText,
+      variables,
+      insertAtElement,
+      callback
+    );
   };
 
   xhr.open("GET", templatePath);
   xhr.send();
+};
+
+export const createElementObject = (
+  rawHTML,
+  variables,
+  insertAtElement,
+  callback = null
+) => {
+  let elementObject = {
+    rawHTML,
+    insertAtElement,
+    callback,
+
+    get element() {
+      let formattedElement = this.rawHTML;
+
+      for (let key in this._variables) {
+        const keyInRes = `$${key}`;
+        formattedElement = formattedElement.replace(
+          keyInRes,
+          this._variables[key]
+        );
+      }
+
+      return formattedElement;
+    },
+
+    get variables() {
+      return this._variables;
+    },
+    set variables(v) {
+      insertAtElement.innerHTML = insertAtElement.innerHTML.replace(
+        this.element.trim(),
+        ""
+      );
+
+      this._variables = v;
+      renderElement(this);
+    },
+  };
+
+  elementObject._variables = variables;
+  renderElement(elementObject);
+
+  if (elementObject.callback !== null) callback(elementObject);
+
+  return elementObject;
+};
+
+export const renderElement = (elementObject) => {
+  elementObject.insertAtElement.innerHTML += elementObject.element;
 };
