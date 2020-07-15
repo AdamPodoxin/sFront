8dd1a928e2f0558984c192f94a3cb903d2f9ef73
Adam Podoxin <adam.podoxin@gmail.com>
2020-07-14 22:01:00 -0700

    Publish examples folder

---
+++ examples/contactList/index.js
@@ -0,0 +1,63 @@
+import * as sFront from "../../sFront.js";
+
+const contactTemplatePath = "contact.html";
+
+let contactPrompt, contactsDiv, nameInput, phoneInput;
+
+let promptMode = "add",
+  editingId = "";
+
+window.onload = () => {
+  contactPrompt = document.querySelector("#contact-prompt");
+  contactsDiv = document.querySelector("#contacts");
+  nameInput = document.querySelector("#name-input");
+  phoneInput = document.querySelector("#phone-input");
+
+  sFront.registerFunctionsInWindow({
+    editContact,
+    deleteContact,
+    addContact,
+    submitContactPrompt,
+  });
+};
+
+const addContact = () => {
+  promptMode = "add";
+  contactPrompt.style.display = "block";
+};
+
+const editContact = (id) => {
+  editingId = id;
+  promptMode = "edit";
+  contactPrompt.style.display = "block";
+
+  let variables = { ...sFront.getElementObjectById(editingId).variables };
+  nameInput.value = variables.name;
+  phoneInput.value = variables.phone;
+};
+
+const deleteContact = (id) => {
+  sFront.deleteElementFromDOMById(id);
+};
+
+const submitContactPrompt = () => {
+  let variables = {
+    name: nameInput.value,
+    phone: phoneInput.value,
+  };
+
+  if (promptMode == "add") {
+    sFront.createElementFromTemplate(
+      contactTemplatePath,
+      variables,
+      contactsDiv
+    );
+  } else if (promptMode == "edit") {
+    sFront.getElementObjectById(editingId).variables = variables;
+  }
+
+  nameInput.value = "";
+  phoneInput.value = "";
+
+  contactPrompt.style.display = "none";
+};
